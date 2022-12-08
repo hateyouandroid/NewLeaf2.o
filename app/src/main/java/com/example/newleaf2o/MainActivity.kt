cd8741bd51be908cec31e65737bd8b72ac4f795e
ashokthorat <vishalvidhate196@gmail.com>
2022-12-08 15:36:31 +0530

    pus first

--- app/src/main/java/com/example/newleaf2o/MainActivity.kt
+++ app/src/main/java/com/example/newleaf2o/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
