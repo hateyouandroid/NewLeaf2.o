f9e942a855c723fe794930933fdc1d629d2583a0
ashokthorat <vishalvidhate196@gmail.com>
2022-12-08 15:37:11 +0530

    push first

--- app/src/main/java/com/example/newleaf2o/MainActivity.kt
+++ app/src/main/java/com/example/newleaf2o/MainActivity.kt
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
