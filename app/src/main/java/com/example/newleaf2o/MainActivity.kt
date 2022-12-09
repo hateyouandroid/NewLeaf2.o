63d390b59308b94d1b5c11b3925974790b2e2f51
OnkarBhandari <Onkarbhandari123@gmail.com>
2022-12-09 22:20:45 +0530

    botto app repo menuc on

--- app/src/main/java/com/example/newleaf2o/MainActivity.kt
+++ app/src/main/java/com/example/newleaf2o/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
           onClick()
+
     }
 
     private fun onClick() {
