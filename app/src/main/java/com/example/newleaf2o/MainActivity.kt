5b82d11abf0ece1f6877d63aab5c65d9490f964d
OnkarBhandari <Onkarbhandari123@gmail.com>
2022-12-09 21:40:54 +0530

    botto app repo menu

--- app/src/main/java/com/example/newleaf2o/MainActivity.kt
+++ app/src/main/java/com/example/newleaf2o/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.newleaf2o
 
+import android.content.ClipData
+import android.content.ClipData.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.newleaf2o.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,7 +14,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+          onClick()
+
+    }
+
+    private fun onClick() {
+        binding.bottomAppBar.setOnItemSelectedListener {
+           when(it){
+               R.id.home->Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show()
+               R.id.cart->Toast.makeText(this,"Cart",Toast.LENGTH_SHORT).show()
+               R.id.profile->Toast.makeText(this,"Profile",Toast.LENGTH_SHORT).show()
+               R.id.menu->Toast.makeText(this,"Menu",Toast.LENGTH_SHORT).show()
 
 
+
+           }
+        }
     }
 }
