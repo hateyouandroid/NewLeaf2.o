2ec41a3b6be9b6bb254977cc2c3e4b27d747f4a6
ashokthorat <vishalvidhate196@gmail.com>
2022-12-12 18:12:37 +0530

    SQLite conectivity

--- app/src/main/java/com/example/newleaf2o/MainActivity.kt
+++ app/src/main/java/com/example/newleaf2o/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.newleaf2o
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,13 +6,21 @@ import com.example.newleaf2o.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: SQLiteHelper
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+      onClick()
+    }
+
+    fun onClick() {
         binding.test.setOnClickListener {
-            val intent = Intent(this,SignUpActivity::class.java)
-            startActivity(intent)
+            val intent = Intent(this, SignUpActivity::class.java)
+           startActivity(intent)
+
         }
     }
 }
+
+
--- app/src/main/java/com/example/newleaf2o/SignUpActivity.kt
+++ app/src/main/java/com/example/newleaf2o/SignUpActivity.kt
@@ -1,14 +1,36 @@
 package com.example.newleaf2o
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.newleaf2o.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var db: SQLiteHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        db = SQLiteHelper(this)
+        binding.btSave.setOnClickListener {
+            val userName = binding.etUsername.text.toString()
+            val mailId = binding.etMail.text.toString()
+            val contactNo = binding.etContact.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (db.insertData(userName,mailId,contactNo,password))
+            {
+                Toast.makeText(this, "Record stored", Toast.LENGTH_LONG).show()
+            }
+            else
+            {
+                Toast.makeText(this, "Record does not stored", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
+
