64a0dcaa21f3fb63e7a036dfa1a295a62ce2bdfe
OnkarBhandari <Onkarbhandari123@gmail.com>
2022-12-20 18:40:24 +0530

    get imei

--- app/src/main/java/com/example/newleaf2o/mainVM.kt
+++ app/src/main/java/com/example/newleaf2o/mainVM.kt
@@ -3,20 +3,7 @@ package com.example.newleaf2o
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.newleaf2o.Data.Request.forgotpass_request
-import com.example.newleaf2o.Data.Request.login_request
-import com.example.newleaf2o.Data.Request.register_request
-import com.example.newleaf2o.Data.Response.forgotpass_response
-import com.example.newleaf2o.Data.Response.login_response
-import com.example.newleaf2o.Data.Response.register_response
 import com.example.newleaf2o.Retrofit.APIClient
-import com.example.newleaf2o.Retrofit.APIService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 open class mainVM : ViewModel() {
     var apiService: APIService = APIClient.getRetrofit().create(APIService::class.java)
--- app/src/main/java/com/example/newleaf2o/retrofit/APIClient.kt
+++ app/src/main/java/com/example/newleaf2o/retrofit/APIClient.kt
@@ -1,28 +1,13 @@
-package com.example.newleaf2o.retrofit
+package com.example.newleaf2o.Retrofit
 
 import com.google.gson.GsonBuilder
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 object APIClient {
     private var BASE_URL: String = "https://neworganicleaf.com/web-admin-login/api/"
+
     private lateinit var retrofit: Retrofit
-    var clients = UnsafeOkHttpClient.unsafeOkHttpClient
-
-
-    private val interceptor = HttpLoggingInterceptor().apply {
-        this.level = HttpLoggingInterceptor.Level.BODY
-    }
-
-    private val client = OkHttpClient.Builder().apply {
-        this.addInterceptor(interceptor)
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(20, TimeUnit.SECONDS)
-            .writeTimeout(25, TimeUnit.SECONDS)
-    }.build()
 
     fun getRetrofit(): Retrofit {
 
@@ -31,7 +16,7 @@ object APIClient {
             .setLenient()
             .create()
 
-        retrofit = Retrofit.Builder().baseUrl(BASE_URL).client(clients)
+        retrofit = Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(gson)).build()
 
         return retrofit
