9b88b4035da9fb0bcb3911c8aa28bb2ae627c074
ashokthorat <vishalvidhate196@gmail.com>
2022-12-22 23:32:46 +0530

    use sharedPreference

--- app/src/main/java/com/example/newleaf2o/LoginActivity.kt
+++ app/src/main/java/com/example/newleaf2o/LoginActivity.kt
@@ -1,11 +1,77 @@
 package com.example.newleaf2o
 
+
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.example.newleaf2o.data.request.LoginResquest
+import com.example.newleaf2o.databinding.ActivityLoginBinding
+import com.example.newleaf2o.retrofit.ApiServices
+import com.example.newleaf2o.vm.LoginVM
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var vm:LoginVM
+    private lateinit var apiServices: ApiServices
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        vm = ViewModelProvider(this)[LoginVM::class.java]
+        val sharedPref = getSharedPreferences("shaPref", MODE_PRIVATE)
+        val editor =sharedPref.edit()
+
+        onClick()
+        observers()
+
+
+//SharedPreference
+        binding.apply {
+            btLogin.setOnClickListener {
+                val mail = etEmail.text.toString()
+                val password = etPass.text.toString()
+
+                editor.apply {
+                    putString("Mail", mail)
+                    putString("Password", password)
+                    apply()
+
+                }
+
+            }
+            /*
+            btLogin.setOnClickListener {
+
+                val sign = sharedPref.getString("Sign",null)
+                val forgot = sharedPref.getString("Forgot",null)
+
+                tvSign.text = sign
+                tvForp.text= forgot
+
+        }*/
+        }
+    }
+
+    private fun observers() {
+    vm.loginResponse.observe(this){
+        Toast.makeText(this,it.ResponseMsg,Toast.LENGTH_LONG).show()
+    }
+   }
+
+    private fun onClick() {
+    binding.btLogin.setOnClickListener{
+        vm.login(loginRequest())
+    }
+    }
+      private fun loginRequest(): LoginResquest{
+        return LoginResquest(
+            "123456765432189",
+            "8833356789",
+            binding.etPass.text.toString().trim()
+
+        )
     }
 }
--- app/src/main/java/com/example/newleaf2o/MainActivity.kt
+++ app/src/main/java/com/example/newleaf2o/MainActivity.kt
@@ -3,26 +3,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.newleaf2o.databinding.ActivityMainBinding
+import com.example.newleaf2o.util.startActivity
 
 class MainActivity : AppCompatActivity() {
-        private lateinit var binding: ActivityMainBinding
-        private lateinit var db: SQLiteHelper
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            onClick()
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: SQLiteHelper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_login)
+
 
     }
 
-    fun onClick() {
-        binding.test.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-
-            startActivity(intent)
-
-        }
-    }
 }
 
 
